005c943f698d6ca20ee20b001f2c04b08b68b2e9
Matt Porritt <mattp@catalyst-au.net>
2022-10-23 15:51:21 +1100

    WIP documenting next steps

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ from code.wifi import wifi
 
 led = machine.Pin("LED", machine.Pin.OUT)
 
-
+# Have a look at implementing the main entry point
+# https://blog.jetbrains.com/pycharm/2018/01/micropython-plugin-for-pycharm/
 try:
     wifi.connect(settings.WIFI['ssid'], settings.WIFI['password'])
 except KeyboardInterrupt:
     machine.reset()
+
+#TODO: MQTT
+# https://github.com/micropython/micropython-lib/tree/master/micropython
+# https://mpython.readthedocs.io/en/master/library/mPython/umqtt.simple.html
+
