072a69d12a463e536ff3b766f7971e304b8eef62
Matt Porritt <pozze30@gmail.com>
2023-02-02 22:36:27 +1100

    initial class and tests stubs for number to image creataion

---
+++ code/imggen/imggen.py
@@ -0,0 +1,7 @@
+import framebuf
+
+
+class ImageGenerator:
+
+    def float_to_image(self, number, size, suffix):
+        return number
---
+++ test/test_imggen.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestImgGen(unittest.TestCase):
+
+    def test_float_to_image(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+    unittest.main()
